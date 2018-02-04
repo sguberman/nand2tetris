e8c366b893a676f1c19188ec4ffdc58d09a83874
Seth Guberman <sguberman@gmail.com>
2018-02-04 16:10:10 -0500

    complete project04 mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,54 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Pseudocode
+//
+// (START)
+//     times = R0
+//     sum = 0
+//
+// (LOOP)
+//     times--
+//     if times < 0:
+//         goto RETURN
+//     sum += R1
+//     goto LOOP
+//
+// (RETURN)
+//     R2 = sum
+//
+// (END)
+//     goto END
+
+(START)
+    @R0
+    D=M
+    @times
+    M=D         // times = R0
+    @sum
+    M=0         // sum = 0
+
+(LOOP)
+    @times
+    M=M-1       // times--
+    D=M
+    @RETURN
+    D;JLT       // if times < 0 goto RETURN 
+    @R1
+    D=M
+    @sum
+    M=D+M       // sum += R1
+    @LOOP
+    0;JMP       // goto LOOP
+
+(RETURN)
+    @sum
+    D=M
+    @R2
+    M=D     // R2 = sum
+
+(END)
+    @END
+    0;JMP
+
